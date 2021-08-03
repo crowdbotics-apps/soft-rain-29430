983226b758424fe2f0cb95f5ca26b86a6e3d77ae
Crowdbotics <team@crowdbotics.com>
2021-08-03 21:31:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "soft_rain_29430Identifier",
+  url: "https://soft_rain_29430Identifier.botics.co",
+  api: "https://soft_rain_29430Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
